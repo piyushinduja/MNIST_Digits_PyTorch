038edd87a1a42c0bed53038cf589ed27d03bcfa8
Piyush Hinduja <74929365+piyushinduja@users.noreply.github.com>
2024-10-24 08:31:18 -0600

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -6,3 +6,12 @@ sudo apt-get install python3 python3-pip python3.10-venv -y
 python3 -m venv dst_env
 source dst_env/bin/activate
 pip3 install torch torchvision
+
+# NVIDIA AND CUDA SETUP
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.6.2/local_installers/cuda-repo-ubuntu2204-12-6-local_12.6.2-560.35.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-6-local_12.6.2-560.35.03-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-6
